aa2da3d8ca9504909460d01d637c1cb5332e18b9
Andrew Lee <andyrewlee@gmail.com>
2015-06-15 19:48:55 -0700

    Allow CORS using rack cors gem

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,8 +1,6 @@
 class FriendsController < ApplicationController
 
   skip_before_filter :verify_authenticity_token
-  after_filter :cors_set_access_control_headers
-  before_filter :cors_preflight_check
 
   def index
     @friends = Friend.all
@@ -13,22 +11,5 @@ class FriendsController < ApplicationController
     Friend.create(name: params[:name], age: params[:age])
     @friends = Friend.all
     render json: @friends
-  end
-
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-    headers['Access-Control-Max-Age'] = "1728000"
-  end
-
-  def cors_preflight_check
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Allow-Headers'] = '*'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :friends
-  match 'friends', to: 'friends#create', via: [:options]
+  get 'friends' => 'friends#index'
+  post 'friends' => 'friends#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
